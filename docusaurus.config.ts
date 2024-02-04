10eb6317db5716992b0adc8e1c542fb614ef6d56
ssshooter <ssshooterx@gmail.com>
2024-02-04 11:29:27 +0800

    docs: update docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -139,7 +139,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Mind Elixir`,
     },
     prism: {
       theme: prismThemes.github,
